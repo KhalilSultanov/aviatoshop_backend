8dceb550387becdaf4095c136ebf689afcc6da82
Khalil <h.sultanoff@yandex.ru>
2024-01-30 21:25:40 +0300

    add goods

--- aviatoProject/apps/shopAviato/admin.py
+++ aviatoProject/apps/shopAviato/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from .models.product import Product, Size, Color, Photos, Review, Category
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title_ru', 'title_az', 'price', 'short_description_ru', 'short_description_az',
                     'full_description_ru', 'full_description_az', 'trading', 'sale', 'category']
@@ -34,6 +35,7 @@ class ProductAdmin(admin.ModelAdmin):
         obj.price = obj.price - (obj.price * obj.sale / 100.0)
 
         super().save_model(request, obj, form, change)
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Size)
